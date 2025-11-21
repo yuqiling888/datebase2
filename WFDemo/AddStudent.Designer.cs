65417c97b2977210c924743860c8cea94b1b1c67
先生 卢 <38904575@qq.com>
2025-11-21 15:20:20 +0800

    11-21

---
+++ WFDemo/AddStudent.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace StudentManager
+{
+    partial class AddStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblStudenName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(199, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lblStudenName
+            // 
+            this.lblStudenName.AutoSize = true;
+            this.lblStudenName.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblStudenName.Location = new System.Drawing.Point(96, 61);
+            this.lblStudenName.Name = "lblStudenName";
+            this.lblStudenName.Size = new System.Drawing.Size(85, 19);
+            this.lblStudenName.TabIndex = 1;
+            this.lblStudenName.Text = "学生姓名";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(96, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "学生姓名";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(199, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // AddStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblStudenName);
+            this.Controls.Add(this.textBox1);
+            this.Name = "AddStudent";
+            this.Text = "AddStudent";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblStudenName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
--- WFDemo/FrmDemo.Designer.cs
+++ WFDemo/FrmDemo.Designer.cs
@@ -33,15 +33,15 @@ namespace StudentManager
             this.BtnGet = new System.Windows.Forms.Button();
             this.lblCurretUser = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnAddStudent = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblnfo
             // 
             this.lblnfo.AutoSize = true;
-            this.lblnfo.Location = new System.Drawing.Point(116, 205);
-            this.lblnfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblnfo.Location = new System.Drawing.Point(87, 164);
             this.lblnfo.Name = "lblnfo";
-            this.lblnfo.Size = new System.Drawing.Size(90, 15);
+            this.lblnfo.Size = new System.Drawing.Size(71, 12);
             this.lblnfo.TabIndex = 0;
             this.lblnfo.Text = "1班学生总数";
             this.lblnfo.Click += new System.EventHandler(this.label1_Click);
@@ -50,10 +50,9 @@ namespace StudentManager
             // 
             this.BtnGet.ForeColor = System.Drawing.SystemColors.ControlText;
             this.BtnGet.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
-            this.BtnGet.Location = new System.Drawing.Point(109, 340);
-            this.BtnGet.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnGet.Location = new System.Drawing.Point(82, 272);
             this.BtnGet.Name = "BtnGet";
-            this.BtnGet.Size = new System.Drawing.Size(237, 68);
+            this.BtnGet.Size = new System.Drawing.Size(178, 54);
             this.BtnGet.TabIndex = 2;
             this.BtnGet.Text = "获取1班和2班学生总数";
             this.BtnGet.UseVisualStyleBackColor = true;
@@ -62,9 +61,10 @@ namespace StudentManager
             // lblCurretUser
             // 
             this.lblCurretUser.AutoSize = true;
-            this.lblCurretUser.Location = new System.Drawing.Point(154, 531);
+            this.lblCurretUser.Location = new System.Drawing.Point(116, 425);
+            this.lblCurretUser.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCurretUser.Name = "lblCurretUser";
-            this.lblCurretUser.Size = new System.Drawing.Size(0, 15);
+            this.lblCurretUser.Size = new System.Drawing.Size(41, 12);
             this.lblCurretUser.TabIndex = 3;
             this.lblCurretUser.Text = "用户名";
             this.lblCurretUser.Click += new System.EventHandler(this.label1_Click_1);
@@ -72,23 +72,33 @@ namespace StudentManager
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(66, 531);
+            this.label1.Location = new System.Drawing.Point(50, 425);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.Size = new System.Drawing.Size(65, 12);
             this.label1.TabIndex = 4;
             this.label1.Text = "当前用户：";
             this.label1.Click += new System.EventHandler(this.label1_Click_2);
             // 
+            // btnAddStudent
+            // 
+            this.btnAddStudent.Location = new System.Drawing.Point(82, 13);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(75, 23);
+            this.btnAddStudent.TabIndex = 5;
+            this.btnAddStudent.Text = "添加学生";
+            this.btnAddStudent.UseVisualStyleBackColor = true;
+            // 
             // FrmDemo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAddStudent);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblCurretUser);
             this.Controls.Add(this.BtnGet);
             this.Controls.Add(this.lblnfo);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmDemo";
             this.Text = "测试前台代码和后台代码分离";
             this.Load += new System.EventHandler(this.FrmDemo_Load);
@@ -103,6 +113,7 @@ namespace StudentManager
         private System.Windows.Forms.Button BtnGet;
         private System.Windows.Forms.Label lblCurretUser;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddStudent;
     }
 }
 
